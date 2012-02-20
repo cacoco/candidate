109aa4ed1fec013e19210bc1765a3413cc24270c
Christopher Coco <cacoco@gmail.com>
2012-02-19 17:52:26 -0800

    Updated

--- app/notifiers/Notifier.java
+++ app/notifiers/Notifier.java
@@ -7,12 +7,14 @@ import play.mvc.Mailer;
 public class Notifier extends Mailer {
 
     private static final String RECIPIENT_EMAIL_ADDRESS =
-            Play.configuration.getProperty("application.email.address");
+            Play.configuration.getProperty("application.email.to.address");
+    private static final String FROM_EMAIL_ADDRESS = 
+            Play.configuration.getProperty("application.email.from.address");
 
     public static void candidate(final Candidate candidate, final boolean previouslySubmitted) {
         setSubject("A new job candidate has applied!");
         addRecipient(RECIPIENT_EMAIL_ADDRESS);
-        setFrom("feedback@flite.com");
+        setFrom(FROM_EMAIL_ADDRESS);
         send(candidate, previouslySubmitted);
     }
 }
